0cec2dee7b3df32643af8dbefc01f88f762b06e1
Kenyka <maratkanovartem89@mail.ru>
2024-10-09 17:24:28 +0300

    change animation mobile bug and change card teams

--- src/app/component/card.tsx
+++ src/app/component/card.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import { OrganizationSwitcher } from "@clerk/nextjs";
+import { useUser } from '@clerk/clerk-react'
 import Image from "next/image";
 
 interface CardProps {
@@ -9,15 +12,17 @@ interface CardProps {
 }
 
 export function Card({ name, description, img, org }: CardProps){
+    const { isSignedIn } = useUser()
+    
     return (
         <section className="flex justify-center md:flex-row flex-col items-center mt-12">
             <Image src={img} width="250" alt="" className="m-2 md:mr-12"/>
             <div className="text-center md:text-left">
                 <h1 className="text-4xl text-white my-5 md:my-2">{name}</h1>
                 <h3 className="text-xl break-word max-w-[500px] text-gray-300 px-4 md:px-0">{description}</h3>
-                {org && (
+                {org && isSignedIn &&(
                     <div className="my-2 text-gray-400 flex flex-col">
-                        Ваши организации: 
+                        Ваши организации:
                         <div className="my-2"><OrganizationSwitcher/></div>
                     </div>
                 )}
--- src/app/component/main.tsx
+++ src/app/component/main.tsx
@@ -6,7 +6,7 @@ export function Main(){
     return (
         <main className="text-center">
           <div className="flex justify-center items-center">
-            <Image src={bannerImg} width="1000" alt="banner" className="banner animate-fade animate-duration-[1500ms]"/>
+            <Image src={bannerImg} width="1000" alt="banner" className="banner animate-fade animate-duration-[700ms] animate-delay-[1000ms]"/>
           </div>
           <h1 className="flex items-center md:flex-row flex-col justify-center text-3xl break-words p-4 text-white animate-fade-up animate-once animate-duration-[1300ms] animate-delay-[1300ms]">
               <span className="text-qual-Q flex items-center mb-2 md:mb-0">
